5fc1b63607e5de11b37f2621ed89c2e45e096685
Mahdi <mahdihs395@gmail.com>
2022-07-29 20:15:04 +0300

    set up the directory of the project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry:{
+    index: './src/index.js',
+    home: './src/home.js',
+    menu: './src/menu.js',
+    contact: './src/contact.js'
+  },
+  output:{
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module:{
+    rules:[
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  }
+}
